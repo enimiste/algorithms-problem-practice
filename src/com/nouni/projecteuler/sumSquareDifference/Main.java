fbb57e915a702a21b29c56abe9e7feaaeaf5327d
Nouni EL Bachir <nouni.elbachir@gmail.com>
2019-10-11 15:18:20 +0100

    Update sum squares difference code

--- src/com/nouni/projecteuler/sumSquareDifference/Main.java
+++ src/com/nouni/projecteuler/sumSquareDifference/Main.java
@@ -1,15 +1,27 @@
 package com.nouni.projecteuler.sumSquareDifference;
 
+import java.util.*;
+
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println(sumSquare(2));
-	}
-	
-	static long sum(int n) {
-		return (long)(n+1)*n/2;
-	}
-	
-	static long sumSquare(int n) {
-		return (long)(n*n*n/3+n*n/2+n/6);
-	}
+    public static void main(String... args) {
+        int n = 100;
+        long sq = sumSquares(n);
+        long qs = squareSum(n);
+        System.out.println(sq);
+        System.out.println(qs);
+        System.out.println(qs - sq);
+    }
+    static long squareSum(int n) {
+        long x = sum(n);
+        return x*x;
+    }
+
+    static long sumSquares(int n) {
+        return Math.round(n*n*n/3.0+n*n/2.0+n/6.0);
+    }
+
+    static long sum(int n) {
+        return n * (n+1)/2;
+    }
 }
